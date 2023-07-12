8e66575540d13c66fa60abb0c00efe5f685b46cd
oriana.phan <oriana.phan@beyondedge.co>
2023-07-12 10:45:21 +0700

    add user login db

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,19 @@ import { UserRepository } from '../user/user.repository';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { JwtService } from '@nestjs/jwt';
+import { UserLoginEntity } from 'src/user_login/user_login.entity';
+import { UserLoginRepository } from 'src/user_login/user_login.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserRepository,
+      UserLoginRepository,
+      UserLoginEntity,
+    ]),
+  ],
   controllers: [UserController],
-  providers: [UserService, UserRepository, JwtService],
+  providers: [UserService, UserRepository, UserLoginRepository, JwtService],
   exports: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,31 @@
 import { UserRepository } from '../user/user.repository';
+import { UserLoginRepository } from '../user_login/user_login.repository';
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { _ } from 'lodash';
 @Injectable()
 export class UserService {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly jwt: JwtService,
+    private readonly userLoginRepository: UserLoginRepository,
   ) {}
 
   async login(email: string, password: string) {
+    console.log(email);
+    console.log(password);
     const hash = await bcrypt.hashSync(password, 9);
     console.log(hash);
     const emailFound = await this.userRepository.checkEmailExist(email);
-    const match = await bcrypt.compare(password, emailFound.password);
+    console.log('isNill', _.isNil(emailFound));
+
     console.log(emailFound);
+
+    if (_.isNil(emailFound)) {
+      return { status: 404, body: { message: 'Email not found!' } };
+    }
+    const match = await bcrypt.compare(password, emailFound.password);
     console.log(match);
     if (!match) {
       return { status: 404, body: { message: 'Email not found!' } };
@@ -34,6 +45,15 @@ export class UserService {
       process.env.REFRESH_TOKEN_SECRET,
       process.env.REFRESH_TOKEN_LIFE,
     );
+    const bodyUser = {
+      refresh_token: refreshToken,
+      access_token: accessToken,
+      user: emailFound,
+    };
+    const newUserLogin = await this.userLoginRepository.createUserLogin(
+      bodyUser,
+    );
+    console.log('newUserLogin', newUserLogin);
     return {
       status: 200,
       body: {
